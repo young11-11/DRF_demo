cbe3300ad22dc92f1e8ff010c2b750d1deb9e517
young11-11 <1264856458@qq.com>
2020-07-12 12:07:31 +0800

    视图集改写RestAPI接口

--- drf_demo/booktest/urls.py
+++ drf_demo/booktest/urls.py
@@ -1,9 +1,14 @@
 from django.urls import re_path
 from . import views
 
-
-
 urlpatterns = [
-    re_path(r'^books/$', views.BookListView.as_view()),
-    re_path(r'^books/(?P<pk>\d+)/$', views.BookDetailView.as_view())
+    re_path(r'^books/$', views.BookInfoViewSet.as_view({
+        "get": "list",
+        "post": "create"
+    })),
+    re_path(r'^books/(?P<pk>\d+)/$', views.BookInfoViewSet.as_view({
+        "get": "retrieve",
+        "put": "update",
+        "delete": "destroy"
+    }))
 ]
--- drf_demo/booktest/views.py
+++ drf_demo/booktest/views.py
@@ -1,9 +1,9 @@
 import json
 
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from django.views import View
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 
@@ -11,65 +11,50 @@ from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
 
 
-class BookListView(ListCreateAPIView):
-    # 指定视图所使用的序列化器类
-    serializer_class = BookInfoSerializer
+class BookInfoViewSet(viewsets.ViewSet):
 
-    # 指定试图所使用的查询集
-    queryset = BookInfo.objects.all()
+    def list(self, request):
+        books = BookInfo.objects.all()
+        serializer = BookInfoSerializer(books,many=True)
+        return Response(serializer.data)
 
-    def get(self, request):
-        """
-        获取所有图书数据：
-        ① 查询数据库获取所有的图书的数据
-        ② 将所有图书的数据通过json进行返回
-        """
-        return self.list(request)
+    def create(self, request):
+        serializer = BookInfoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def post(self, request):
-        """
-        新增一本图书数据：
-        ① 获取参数并进行校验
-        ② 创建图书对象并保存到数据库
-        ③ 将新增图书数据通过json进行返回
-        """
-        return self.create(request)
+    def retrieve(self, request, pk):
 
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except  BookInfo.DoesNotExist:
+            raise Http404
 
-class BookDetailView(RetrieveUpdateDestroyAPIView):
-    # 指定视图所使用的序列化器类
-    serializer_class = BookInfoSerializer
+        serializer = BookInfoSerializer(book)
+        return Response(serializer.data)
 
-    # 指定视图所使用的查询集
-    queryset = BookInfo.objects.all()
+    def update(self, request, pk):
 
-    def get(self, request, pk):
-        """
-        self.kwargs：字典，保存的是从url地址提取的所有的命名参数
-        获取指定图书数据(根据pk)：
-        ① 根据pk去数据库查询指定的图书数据
-        ② 将图书数据通过json数据进行返回
-        """
-        return self.retrieve(request, pk)
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except  BookInfo.DoesNotExist:
+            raise Http404
 
-    def put(self, request, pk):
-        """
-        修改指定图书数据(根据pk)：
-        ① 根据pk去数据库查询指定的图书数据
-        ② 获取参数并进行校验
-        ③ 修改图书数据并保存到数据库
-        ④ 将修改图书的数据通过json数据进行返回
-        """
-        return self.update(request, pk)
+        serializer = BookInfoSerializer(book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
-    def delete(self, request, pk):
-        """
-        删除指定图书数据(根据pk)：
-        1. 根据pk去数据库查询指定的图书数据
-        2. 删除指定图书数据
-        3. 返回响应，status=204
-        """
-        return self.destroy(request, pk)
+    def destroy(self, request, pk):
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except BookInfo.DoesNotExist:
+            raise Http404
+
+        book.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # list：获取一组数据的通用代码
 # create：新增一个数据的通用代码
