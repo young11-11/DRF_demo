47be259339e2ce42ae1d83db27a525ab080498e7
young11-11 <1264856458@qq.com>
2020-07-12 21:10:29 +0800

    自定义权限控制类

--- drf_demo/booktest/views.py
+++ drf_demo/booktest/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse, HttpResponse, Http404
 from django.views import View
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.response import Response
 from rest_framework import status, viewsets, mixins
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -18,6 +18,21 @@ from booktest.models import BookInfo
 from booktest.serializers import BookInfoSerializer
 
 
+
+class MyPermission(BasePermission):
+    def has_permission(self, request, view):
+        """判断对使用此权限类的视图是否有访问权限"""
+        # 任何用户对使用此权限类的视图都有访问权限
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        """判断对使用此权限类视图中的某个数据对象是否有访问权限"""
+        # 需求: 对id为1，3的数据对象有访问权限
+        if obj.id in (1, 3):
+            return True
+        return False
+
+
 class BookInfoViewSet(ReadOnlyModelViewSet):
     # 指定序列化器类
     serializer_class = BookInfoSerializer
@@ -27,8 +42,8 @@ class BookInfoViewSet(ReadOnlyModelViewSet):
     # 指定当前视图自己的认证方案，不再使用全局认证方案
     authentication_classes = [SessionAuthentication]
 
-    # 指定当前视图自己的权限控制方式，不再使用全局权限控制方式
-    permission_classes = [IsAuthenticated]
+    # 使用自定义的权限控制类
+    permission_classes = [MyPermission]
 
 
 
